1ca858a3e9ecd9be555fd168067f84de0fdb687d
yashUcr773 <yagga004@ucr.edu>
2024-08-09 00:14:08 +0530

    formatting and spacing issues

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -6,11 +6,11 @@ import { DUMMY_USERS } from './components/user/dummy-users';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'first-angular-app';
--- first-angular-app/src/app/components/header/header.component.ts
+++ first-angular-app/src/app/components/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  standalone: true,
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent { }
