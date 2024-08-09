176a88a3771d711a592b1213c43440f776e76860
yashUcr773 <yagga004@ucr.edu>
2024-08-09 13:33:42 +0530

    pipes

--- first-angular-app/src/app/components/tasks/task/task.component.ts
+++ first-angular-app/src/app/components/tasks/task/task.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../../../utils/types.utils';
 import { CardComponent } from "../../ui/card/card.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-task',
     standalone: true,
-    imports: [CardComponent],
+    imports: [CardComponent, DatePipe],
     templateUrl: './task.component.html',
     styleUrl: './task.component.scss'
 })
