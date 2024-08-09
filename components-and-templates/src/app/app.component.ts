1958bcbbca17b96238e2a3ff34952d23d0595f81
yashUcr773 <yagga004@ucr.edu>
2024-08-09 19:31:10 +0530

    split into basic components.

--- components-and-templates/src/app/app.component.ts
+++ components-and-templates/src/app/app.component.ts
@@ -1,41 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { ServerStatusComponent } from "./components/dashboard/server-status/server-status.component";
+import { TrafficComponent } from "./components/dashboard/traffic/traffic.component";
+import { TicketsComponent } from "./components/dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./components/dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent]
 })
 export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
+
 }
