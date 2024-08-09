44a91b20f34372855930716fd6ceee560a6086ed
yashUcr773 <yagga004@ucr.edu>
2024-08-09 21:06:25 +0530

    splitting styles into respective components

--- components-and-templates/src/app/components/shared/button/button.component.ts
+++ components-and-templates/src/app/components/shared/button/button.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 
 @Component({
     selector: 'button[appButton]',
     standalone: true,
     imports: [],
     templateUrl: './button.component.html',
-    styleUrl: './button.component.css'
+    styleUrl: './button.component.css',
+
 })
 export class ButtonComponent {
 
