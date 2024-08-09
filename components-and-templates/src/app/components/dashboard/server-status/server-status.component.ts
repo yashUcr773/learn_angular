620e763d7af3bda195ab28ad485db556e9e0158f
yashUcr773 <yagga004@ucr.edu>
2024-08-09 22:05:22 +0530

    update server status randomly

--- components-and-templates/src/app/components/dashboard/server-status/server-status.component.ts
+++ components-and-templates/src/app/components/dashboard/server-status/server-status.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-server-status',
-  standalone: true,
-  imports: [],
-  templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+    selector: 'app-server-status',
+    standalone: true,
+    imports: [],
+    templateUrl: './server-status.component.html',
+    styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-    currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+    currentStatus: 'online' | 'offline' | 'unknown' = 'online';
 
+    ngOnInit() {
+        setInterval(() => {
+            this.currentStatus = Math.random() > 0.5 ? 'online' : Math.random() < 0.9 ? 'offline' : 'unknown';
+        }, 5000)
+    }
 }
