9d6466fadfc14a1fca63d4d532a1f993e1e2f22c
yashUcr773 <yagga004@ucr.edu>
2024-08-12 02:16:24 +0530

    HTTP interceptor

--- HTTP/src/main.ts
+++ HTTP/src/main.ts
@@ -1,7 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 
-bootstrapApplication(AppComponent, { providers: [provideHttpClient()] }).catch((err) => console.error(err));
+function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
+    console.log('Request:', request);
+    const req = request.clone({
+        // headers: request.headers.set('X-debug', 'true')
+    });
+    return next(req)
+}
+
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideHttpClient(
+            withInterceptors([loggingInterceptor])
+        )
+    ]
+}).catch((err) => console.error(err));
