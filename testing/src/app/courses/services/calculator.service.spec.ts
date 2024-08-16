9d8c3fb3192ae301d78ad1981b998ce7842fa116
yashUcr773 <yagga004@ucr.edu>
2024-08-16 13:01:05 +0530

    Test bed, focus on test, disable a test

--- testing/src/app/courses/services/calculator.service.spec.ts
+++ testing/src/app/courses/services/calculator.service.spec.ts
@@ -1,22 +1,32 @@
 import { CalculatorService } from "./calculator.service";
+import { TestBed } from '@angular/core/testing'
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
     let calculatorService: CalculatorService;
-    let loggerSpy: any;
+    let loggerSpy: jasmine.SpyObj<LoggerService>;
 
     beforeEach(() => {
         loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-        calculatorService = new CalculatorService(loggerSpy);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                { provide: LoggerService, useValue: loggerSpy }
+            ]
+        });
+
+        calculatorService = TestBed.inject(CalculatorService);
     });
 
-    it('should add two numbers', () => {
+    xit('should add two numbers', () => {
 
         const result = calculatorService.add(4, 7);
         expect(result).withContext('Addition Failed').toBe(11);
         expect(loggerSpy.log).withContext('Logger invoked multiple times').toHaveBeenCalledTimes(1);
     });
 
-    it('should subtract two numbers', () => {
+    fit('should subtract two numbers', () => {
 
         const result = calculatorService.subtract(4, 7);
         expect(result).withContext('Subtraction Failed').toBe(-3);
