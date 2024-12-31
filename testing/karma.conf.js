a170edd5c01665f22bc7db2776056f0708c20961
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2024-12-31 13:34:59 +0530

    update browser flags

--- testing/karma.conf.js
+++ testing/karma.conf.js
@@ -26,8 +26,14 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['ChromeHeadless'],
+        browsers: ['ChromeHeadlessNoSandbox'],
         singleRun: false,
-        restartOnFileChange: true
+        restartOnFileChange: true,
+        customLaunchers: {
+            ChromeHeadlessNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox', '--disable-gpu']
+            }
+        }
     });
 };
